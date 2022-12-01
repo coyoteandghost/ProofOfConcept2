8d2bd72784ed23d671425ae938ad8d79cec7e5ec
kenneth <kc4338@nyu.edu>
2022-12-01 11:59:11 -0500

    working with physics

--- Anything and Everything 2/Assets/scripts/playerMove.cs
+++ Anything and Everything 2/Assets/scripts/playerMove.cs
@@ -7,6 +7,7 @@ public class playerMove : MonoBehaviour
     public CharacterController controllerP;
     public float speed;
     public float jumpHeight = 3f;
+    public float drag;
 
     public Transform groundCheck; // spherecheck for grav reset
     public float groundDistance = 0.4f;
@@ -23,30 +24,21 @@ public class playerMove : MonoBehaviour
 
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); // creates invisible sphere w position, radius, and layer to check for collision
+        currentVelocity.z = 0;
 
-        if (isGrounded) //if grounded, reset velocity
+        // creates invisible sphere w position, radius, and layer to check for collision
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); 
+
+        if (isGrounded && currentVelocity.y <0) //if grounded, reset velocity
         {
-            if (isBounced)
-            {
-
-            }
-            else if (currentVelocity.y < 0)
-            {
-                currentVelocity.y = -2f;
-            }
-
-
-
-            
+            currentVelocity.y = -2f;
         }
-        //if (isBounced && )
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * 0; //controlling the movement in the forward and side to side
-        controllerP.Move(move * speed * Time.deltaTime); //take the movement formula and apply it to character with respect to time and speed
+        //controllerP.Move(move * speed * Time.deltaTime); //take the movement formula and apply it to character with respect to time and speed
 
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
@@ -56,23 +48,27 @@ public class playerMove : MonoBehaviour
         //adding gravity downwards!
         currentVelocity.y += grav * Time.deltaTime * weight;
 
-        var flags = controllerP.Move(currentVelocity * Time.deltaTime); //tells where it collides with things
+        var flags = controllerP.Move((currentVelocity + move*speed )* Time.deltaTime); //tells where it collides with things
         if ((flags & CollisionFlags.Above)!=0)
         {
             currentVelocity.y = 0;
         }
-
-        Debug.Log(isGrounded);
-    }
-    
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "bounce")
+        /*if ((flags & CollisionFlags.Below) != 0)
         {
-            isBounced = true;
+            currentVelocity.y = -currentVelocity.y;
+        }*/
+
+        currentVelocity.x *= Mathf.Clamp01(1f - drag * Time.deltaTime);
+
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.sharedMaterial.bounciness > 0)
+        {
+            currentVelocity = Vector3.Reflect(currentVelocity, hit.normal) * hit.collider.sharedMaterial.bounciness;
         }
     }
-    
 
 
 }
