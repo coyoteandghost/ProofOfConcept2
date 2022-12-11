46af9c98525eae7aeaff3239e16875e58d411e68
Miles <mae9775@nyu.edu>
2022-12-11 16:40:07 -0500

    set design in room 2 complete

--- Anything and Everything 2/Assets/scripts/OneWayPlatform.cs
+++ Anything and Everything 2/Assets/scripts/OneWayPlatform.cs
@@ -8,7 +8,7 @@ public class OneWayPlatform : MonoBehaviour
     public Collider col;
     void Start()
     {
-        
+        player = FindObjectOfType<playerMove>().transform;
     }
 
     void Update()
